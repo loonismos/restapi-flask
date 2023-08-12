887935f0e7040ecbac1d4aa406f72195d116a031
aggie <aggie@pop-os.localdomain>
2023-08-11 22:42:50 -0300

    test: adding unit test with pytest

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import mongomock
 
 
 class DevConfig():
@@ -8,4 +9,13 @@ class DevConfig():
         'host': os.getenv('MONGODB_HOST'),
         'username': os.getenv('MONGODB_USERNAME'),
         'password': os.getenv('MONGODB_PASSWORD')
+    }
+
+
+class MockConfig():
+
+    MONGODB_SETTINGS = {
+        'db': 'users',
+        'host': 'mongodb://localhost',
+        'mongo_client_class': mongomock.MongoClient
     }
